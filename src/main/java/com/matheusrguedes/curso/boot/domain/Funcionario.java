9f129036d8b7310c1cef3310a908d7bed48b9d26
Matheus Rodrigues Guedes <matheusguedes25@gmail.com>
2020-07-15 20:56:53 -0300

    Modificações nas classes Funcionário e Cargo

--- src/main/java/com/matheusrguedes/curso/boot/domain/Funcionario.java
+++ src/main/java/com/matheusrguedes/curso/boot/domain/Funcionario.java
@@ -24,7 +24,7 @@ public class Funcionario extends AbstractEntity<Long> {
 	@Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
 	private LocalDate dataEntrada;
 	
-	@Column(name = "data_saida", nullable = false, columnDefinition = "DATE")
+	@Column(name = "data_saida", columnDefinition = "DATE")
 	private LocalDate dataSaida;
 	
 	@OneToOne(cascade = CascadeType.ALL)
