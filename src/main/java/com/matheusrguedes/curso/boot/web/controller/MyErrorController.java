7bb33ae56fc164f8156ab41e6501865ebc19f993
Matheus Rodrigues Guedes <matheusguedes25@gmail.com>
2020-09-26 14:53:51 -0300

    Criação do controlador de erros e as páginas.

--- src/main/java/com/matheusrguedes/curso/boot/web/controller/MyErrorController.java
+++ src/main/java/com/matheusrguedes/curso/boot/web/controller/MyErrorController.java
@@ -1,11 +1,13 @@
 package com.matheusrguedes.curso.boot.web.controller;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /*
  * Classe que substituirá o controlador padrão do spring boot para erros, através da interface ErrorController.
@@ -17,16 +19,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * 
  * */
 
-//@Controller
+@Controller
 public class MyErrorController implements ErrorController {
 
-	@RequestMapping( path = "${server.error.path}", method = RequestMethod.GET)
-	private String handleError(HttpServletRequest request, ModelMap model) {		
+	@RequestMapping("/error")
+	private String handleError(HttpServletRequest request, ModelMap model) {
+		
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		Object message = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+		
+		if (status != null) {
+			
+			Integer statusCode = Integer.valueOf(status.toString());
+			
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				return "error-404";
+			}
+			
+			else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				String errorMessage = message.toString();
+				model.addAttribute("message", errorMessage.split(";")[1]);
+				model.addAttribute("status", statusCode);
+				return "error-500";
+			}
+		}
+		
 		return "error";
 	}
 	
 	@Override
 	public String getErrorPath() {
-		return "/error";
+		return null;
 	}
 }
